eda3dc2f31e76fa52427a6d7a8c095420dbac85c
Alexandra Eremia <alexandra.eremia.06@gmail.com>
2025-12-09 12:31:51 +0100

    Update gwz_sales.sql

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,6 +1,7 @@
-SELECT 
-date_date
-SUM(turnover) AS turnover
+SELECT
+date_date,
+SUM(turnover) AS turnover,
+SUM(purchase_cost) AS total_purchase_cost
 FROM `data-analytics-ae.course14_AE.gwz_sales`
 GROUP BY date_date
 ORDER BY date_date;
