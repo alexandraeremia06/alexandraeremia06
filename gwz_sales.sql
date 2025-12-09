076fad8ca6651ce53f09c5a97936632827ef3590
Alexandra Eremia <alexandra.eremia.06@gmail.com>
2025-12-09 14:32:09 +0100

    round updates
    rounding the numbers to 2 decimals

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,6 +1,7 @@
-SELECT 
-date_date
-SUM(turnover) AS turnover
+SELECT
+date_date,
+ROUND(SUM(turnover), 2) AS turnover,
+ROUND(SUM(purchase_cost), 2) AS total_purchase_cost
 FROM `data-analytics-ae.course14_AE.gwz_sales`
 GROUP BY date_date
 ORDER BY date_date;
